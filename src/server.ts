dab1f23b576959379ff781ff891ef35154d8f080
cayo <cayofonseca12@gmail.com>
2025-09-01 21:53:35 -0300

    feat(userRoutes): add POST /register route and connect to server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { environment } from "./config/environment";
+import registerRoutes from "./routes/registerRoutes";
 
 const server = express();
 
 server.use(express.json());
+
+server.use("/register", registerRoutes);
 
 server.listen(environment.port, () => {
     console.log(`O servidor está rodando na porta ${environment.port}`);
