e58b55882eb8b1bd5eb50a1ea5baabbb644331a7
cayo <cayofonseca12@gmail.com>
2025-09-01 21:28:49 -0300

    feat(userRepository): add a findById method

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -5,4 +5,13 @@ export const userRepository = {
     create: async (data: Prisma.UserCreateInput) => {
         return await prisma.user.create({ data });
     },
+    findByEmail: async (email: string) => {
+        return await prisma.user.findUnique({ where: { email } });
+    },
+    findById: async (id: number) => {
+        return await prisma.user.findUnique({ where: { id } });
+    },
+    getAll: async () => {
+        return await prisma.user.findMany();
+    },
 };
