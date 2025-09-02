6ecb6cc8cdef83909d36e30906a94c81a662f6b8
cayo <cayofonseca12@gmail.com>
2025-09-01 21:45:00 -0300

    feat(userController): add a register method

---
+++ src/controllers/userController.ts
@@ -0,0 +1,15 @@
+import { userService } from "../services/userService";
+import { Request, Response } from "express";
+
+export const userController = {
+    register: async (req: Request, res: Response) => {
+        const { name, email, password } = req.body;
+        if (!name || !email || !password) {
+            return res
+                .status(400)
+                .json({ message: "Nome, email e senha são obrigatórios" });
+        }
+        const newUser = await userService.register(name, email, password);
+        return res.status(201).json(newUser);
+    },
+};
