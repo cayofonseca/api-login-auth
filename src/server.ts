02ae9c7920cc417b8c26dc5b3558d9ea404f7693
cayo <cayofonseca12@gmail.com>
2025-09-01 22:04:42 -0300

    feat(getAllUserRoutes): create a GET /users and connect to server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import { environment } from "./config/environment";
 import registerRoutes from "./routes/registerRoutes";
+import getAllUsersRoutes from "./routes/getAllUsersRoutes";
 
 const server = express();
 
 server.use(express.json());
 
 server.use("/register", registerRoutes);
+server.use("/users", getAllUsersRoutes);
 
 server.listen(environment.port, () => {
     console.log(`O servidor está rodando na porta ${environment.port}`);
