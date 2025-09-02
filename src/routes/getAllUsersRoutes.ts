6c5f86c750918da537fc847257dab742e6ea21e4
cayo <cayofonseca12@gmail.com>
2025-09-01 22:59:04 -0300

    feat(usersRoutes): protect GET /users with JWT

--- src/routes/getAllUsersRoutes.ts
+++ src/routes/getAllUsersRoutes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { userController } from "../controllers/userController";
+import passport from "passport";
 
 const router = Router();
 
-router.get("/", userController.getAll);
+router.get(
+    "/",
+    passport.authenticate("jwt", { session: false }),
+    userController.getAll
+);
 
 export default router;
