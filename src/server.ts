453775320347a4b020e3401bad21ecef3b66fda7
cayo <cayofonseca12@gmail.com>
2025-09-01 22:41:40 -0300

    feat(loginRoutes): add a POST \login and connect to server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { environment } from "./config/environment";
 import registerRoutes from "./routes/registerRoutes";
 import getAllUsersRoutes from "./routes/getAllUsersRoutes";
+import loginRoutes from "./routes/loginRoutes";
 
 const server = express();
 
@@ -9,6 +10,7 @@ server.use(express.json());
 
 server.use("/register", registerRoutes);
 server.use("/users", getAllUsersRoutes);
+server.use("/login", loginRoutes);
 
 server.listen(environment.port, () => {
     console.log(`O servidor está rodando na porta ${environment.port}`);
