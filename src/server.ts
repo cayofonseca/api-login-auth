016a131b704c0d40f487118904d00044ef8c49bf
cayo <cayofonseca12@gmail.com>
2025-09-04 11:16:39 -0300

    feat(server): add a Put /update/id in the server

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import { environment } from "./config/environment";
 import registerRoutes from "./routes/registerRoutes";
 import getAllUsersRoutes from "./routes/getAllUsersRoutes";
 import loginRoutes from "./routes/loginRoutes";
+import updateUserRoutes from "./routes/updateUserRoutes";
 import passport from "./middleware/passportStrategy";
 
 const server = express();
@@ -13,6 +14,7 @@ server.use(passport.initialize());
 server.use("/register", registerRoutes);
 server.use("/users", getAllUsersRoutes);
 server.use("/login", loginRoutes);
+server.use("/users", updateUserRoutes);
 
 server.listen(environment.port, () => {
     console.log(`O servidor está rodando na porta ${environment.port}`);
